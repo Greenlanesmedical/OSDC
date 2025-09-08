84dd25bd35c9f1ab8e3a1cc7bedfbd1976cfd51e
Greenlanesmedical <greenlanesmedical@gmail.com>
2025-09-08 17:06:42 +0100

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,8 @@ const cormorant = Cormorant_Garamond({ subsets: ['latin'], weight: ['400','700']
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className={`${inter.variable} ${cormorant.variable}`}>
-      <a href=\"#main\" className=\"sr-only focus:not-sr-only fixed top-2 left-2 z-\[1000] bg-black text-white px-3 py-2 rounded\">Skip to content</a>
-      <Component {...pageProps} />
+      <a href="#main" className="sr-only focus:not-sr-only fixed top-2 left-2 z-[1000] bg-black text-white px-3 py-2 rounded">Skip to content</a>
+<Component {...pageProps} />
       <ConsentManager />
     </div>
   );
